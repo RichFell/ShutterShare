0446f5ed77cca52eb3c66fa5a9e11991f06ddec4
Rich Fellure <rfellure24@gmail.com>
2014-06-09 16:10:40 -0500

    First Commit

---
+++ ShutterShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShutterShare
+//
+//  Created by Richard Fellure on 6/9/14.
+//  Copyright (c) 2014 Rich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShutterShare/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShutterShare
+//
+//  Created by Richard Fellure on 6/9/14.
+//  Copyright (c) 2014 Rich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
